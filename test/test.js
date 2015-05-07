16590f918dd9a25c6153edd4c246b4e4628dc7c6
Isaac <jico@justjico.com>
2015-05-06 23:51:52 -0600

    adding mocha tests

--- test/test.js
+++ test/test.js
@@ -1,13 +1,32 @@
+var assert = require("assert"); // node.js core module
 
-var seneca = require('seneca')();
+describe('redis',function(){
 
-seneca.use('..')
-    .listen();
+    var seneca = require('seneca')();
+    seneca.use('..');
 
-seneca.act( {lpi:'redis', cmd:'info', redis:{host:'localhost',port:6379}}, function(err,result){
-    console.log( '%j', result );
+    describe('info',function(){
+        it('should connect and get info from server',function(done){
+            seneca.act( {lpi:'redis', cmd:'info', redis:{host:'localhost',port:6379}}, function(err,result){
+                console.log( '%j', result );
+                assert.isObject(result,'result is an object');
+                done();
+            });
+        })
+    });
+
+    describe('list',function(){
+        it('should return a command\'s json schema',function(done){
+            seneca.act({lpi:'redis',cmd:'list'}, function(err,list){
+                console.log('%j',list);
+                assert.isObject(list,'list is object');
+                done();
+            });
+        });
+    });
 });
 
-seneca.act({lpi:'redis',cmd:'list'}, function(err,result){
-    console.log('%j',result);
-});
+
+
+
+
